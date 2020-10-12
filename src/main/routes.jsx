18691f76b7ed354d724c9fb0a18d8f0f14c7bc6c
Larissa Amaral <larissa95.amaral@gmail.com>
2020-10-12 16:58:42 -0300

    cria component para chart de geolocalizacao

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -3,11 +3,13 @@ import { HashRouter, Route, Redirect } from 'react-router-dom'
 
 import Relatorio from '../ocorrencia/ocorrencia-relatorio'
 import Form from '../ocorrencia/ocorrencia-form'
+import Geo from '../ocorrencia/ocorrencia-geo'
 
 export default props => (
     <HashRouter>
         <Route path='/relatorios' component={Relatorio} />
         <Route path='/reportar' component={Form} />
-        <Redirect from='*' to='/relatorios' />
+        <Route path='/' component={Geo} />
+        <Redirect from='*' to='/' />
     </HashRouter>
 )
